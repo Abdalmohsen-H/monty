0f4881f554f4c31831ac34a190c9fb2eae23ebd8
msudany <mssodany2002@gmail.com>
2023-06-22 20:04:53 +0300

    Style mntycmd_hlpr0.c

--- mntycmd_hlpr0.c
+++ mntycmd_hlpr0.c
@@ -1,9 +1,15 @@
 #include "monty.h"
+/**
+ * perrpsh - returns an error for push
+ * @ln_numbr: line number where the error is present
+ *
+ * Return: void
+ */
 void perrpsh(unsigned int ln_numbr)
 {
 	fprintf(stderr, "L%u: usage: push integer\n", ln_numbr);
-        fclose(globlData.zfile);   
+	fclose(globlData.zfile);
 	/*free list first*/
 	free_dlistint(globlData.head);
-	exit(EXIT_FAILURE);	
+	exit(EXIT_FAILURE);
 }
